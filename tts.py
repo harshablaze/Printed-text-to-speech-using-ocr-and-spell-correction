3c86ec2f4d86571649d4b416be2b6c3321900e16
harshablaze <harsha.mullapudi222@gmail.com>
2021-03-24 10:59:24 +0530

    modified tts

--- tts.py
+++ tts.py
@@ -10,6 +10,20 @@ text = text.replace('\n',' ')
 
 #speak out the text read
 speaker.Speak(text)
+
 #print(text)
 #close file
 inFile.close()
+
+'''
+# importing the pyttsx library
+import pyttsx3
+
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+engine.say("My first code on text-to-speech")
+engine.runAndWait()
+
+'''
--- tts2.py
+++ tts2.py
@@ -1,39 +1,15 @@
-"""
-import pyttsx
-# import engineio #engineio module is not needed.
+# importing the pyttsx library
+import pyttsx3
 
-engineio = pyttsx.init()
-voices = engineio.getProperty('voices')
-engineio.setProperty('rate', 130)    # Aquí puedes seleccionar la velocidad de la voz
-engineio.setProperty('voice',voices[1].id)
+# initialisation
+engine = pyttsx3.init('sapi5')
+inFile=open('text.txt', "r")
+text=inFile.read()
 
-def speak(text):
-    engineio.say(text)
-    engineio.runAndWait()
+#remove new line tags
+text = text.replace('\n',' ')
 
-speak("What do you want me to say?")
-while(1):
-    phrase = input("--> ")
-    if (phrase == "exit"):
-        exit(0)
-    speak(phrase)
-    print(voices)
-"""
+# testing
+engine.say(text)
+engine.runAndWait()
 
-from espeakng import ESpeakNG
-esng = ESpeakNG()
-esng.say('Hello World')
-"""
-esng.pitch = 32 esng.speed = 150 esng.say('Hello World!')
-esng.voice = ‘german’ esng.say('Hallo Welt!')
-"""
-"""
-import wave
-import io
-from espeak import ESpeakNG
-esng=ESpeakNG()
-esng.voice='english-us'
-wavs = esng.synth_wav('Hello World!')
-wav = wave.open(StringIO.StringIO(wavs))
-print(wav.getnchannels(), wav.getframerate(), wav.getnframes())
-"""
