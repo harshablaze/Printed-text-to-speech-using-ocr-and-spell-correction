fd4f7aad238f6539295dae37165629352259a71d
harshablaze <harsha.mullapudi222@gmail.com>
2021-03-25 11:28:56 +0530

    updated env, requirements, tesseract location and readme file

--- ocr.py
+++ ocr.py
@@ -4,13 +4,19 @@ import pytesseract
 #not needed
 import cv2
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+#pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'.\Tesseract-OCR\tesseract.exe'
+#use above path when tesseract is moved to current project directory
 im = Image.open('test2-3.jpg')
 #sample4.jpg
 
 # Example config: r'--tessdata-dir "C:\Program Files (x86)\Tesseract-OCR\tessdata"'
 # It's important to add double quotes around the dir path.
-tessdata_dir_config = r'--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+
+#tessdata_dir_config = r'--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+tessdata_dir_config = r'--tessdata-dir ".\Tesseract-OCR\tessdata"'
+#use above path when tesseract is moved to current project directory
+
 text = pytesseract.image_to_string(im, lang='eng', config=tessdata_dir_config)
 
 #text = pytesseract.image_to_string(im, lang = 'eng')
--- readme.txt
+++ readme.txt
@@ -1,7 +1,13 @@
-install pytesseract
-install pillow
-install opencv-python
-install tesseract
-install gtts
-install matplotlib
-install textblob
+#after installing the project repo
+#activate the env
+.\env\Scripts\activate
+
+#or
+#install requirements.txt
+pip install -r requirements.txt
+
+#to save the packages used
+pip freeze>requirements.txt
+
+#to start the project
+python start.py
