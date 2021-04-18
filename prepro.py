57afa9bfbbb09f03abb78eabbb4ad3fb7a6c7950
harshablaze <harsha.mullapudi222@gmail.com>
2021-04-18 13:35:38 +0530

    imshow corrected in prepro

--- prepro.py
+++ prepro.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # path to input image is specified and
 # image is loaded with imread command
-image1 = cv2.imread('sample4.jpg')
+image1 = cv2.imread('images/sample4.jpg')
 
 # cv2.cvtColor is applied over the
 # image input with applied parameters
@@ -25,23 +25,23 @@ ret,thresh4 = cv2.threshold(thresh1,127,255,cv2.THRESH_BINARY)
 # the window showing output images
 # with the corresponding thresholding
 # techniques applied to the input image
-cv2.imshow('Adaptive Mean', thresh1)
-cv2.imshow('Adaptive Gaussian', thresh2)
-cv2.imshow('binary inv', thresh3)
-cv2.imshow('binary', thresh4)
+#cv2.imshow('Adaptive Mean', thresh1)
+#cv2.imshow('Adaptive Gaussian', thresh2)
+#cv2.imshow('binary inv', thresh3)
+#cv2.imshow('binary', thresh4)
 
-cv2.imwrite("test2-1.jpg", thresh1)
-cv2.imwrite("test2-2.jpg", thresh2)
-cv2.imwrite("test2-3.jpg", thresh3)
-cv2.imwrite("test2-4.jpg", thresh4)
+#cv2.imwrite("test2-1.jpg", thresh1)
+#cv2.imwrite("test2-2.jpg", thresh2)
+cv2.imwrite("images/test2-3.jpg", thresh3)
+#cv2.imwrite("test2-4.jpg", thresh4)
 
 #thresholding completed
 
 #noise removal code
 # Median blurring
 medBlur = cv2.medianBlur(thresh1,5)
-cv2.imshow('Media Blurring', medBlur)
-cv2.imwrite("test2-1-1.jpg", medBlur)
+#cv2.imshow('Media Blurring', medBlur)
+#cv2.imwrite("images/test2-1-1.jpg", medBlur)
 
 # De-allocate any associated memory usage
 if cv2.waitKey(0) & 0xff == 27:
